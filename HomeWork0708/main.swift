1aa9601db43b1fd668cdb5df9930bd8522976661
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 <bbeka125@gmail.com>
2022-05-06 16:58:40 +0600

    Initial Commit

---
+++ HomeWork0708/main.swift
@@ -0,0 +1,38 @@
+
+//1. Составить замыкание, которое считает кол-во букв в нашем предложении, которое мы вводим в консоли.
+//2. Составить замыкание, которое переводит наши деньги в курс доллара.
+//3. Составить программу с помощью функции sorted(), которая cортирует список имен по кол-ву букв по возрастанию.
+//
+func firstTask() {
+print("Введите что - нибудь")
+let string = readLine()
+let res = string.map { char in string?.count}
+print(res!!)
+}
+firstTask()
+
+
+var operation: (Float) -> Float
+operation = { $0 * 82.10}
+var result = operation(500)
+print(result)
+
+let names = ["Beka", "Tala", "Sultan",  "Danil", "Asely"]
+let namesSorted = names.sorted {$0.count > $1.count }
+print(namesSorted)
+
+
+
+
+
+var array = [3, 5, 7, 9, 11]
+
+var sqrtOfNumbers = array.map { $0 * $0
+}
+let result1 = sqrtOfNumbers
+print(sqrtOfNumbers)
+
+for sqrt in array {
+    let resultSqrt = sqrt * sqrt
+    print(resultSqrt)
+}
